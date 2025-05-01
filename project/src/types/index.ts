afcf940c29bf10475906b4e5ff20880cbe1baaa1
Tu Nombre <usuario@ejemplo.com>
2025-04-30 23:16:30 -0300

    refactor: Move ModalProps interface to types directory and update Modal component imports

--- project/src/types/index.ts
+++ project/src/types/index.ts
@@ -4,6 +4,7 @@ import { Task, TaskContextType } from "./tasks/taskTypes";
 import { TaskItemProps } from "./tasks/taskItemTypes";
 import { User } from "./userTypes";
 import { AuthContextType } from "./auth/authContextTypes";
+import { ModalProps } from "./modalTypes";
 
 export type {
   LoginFormProps,
@@ -12,7 +13,8 @@ export type {
   TaskContextType,
   TaskItemProps,
   User,
-  AuthContextType
+  AuthContextType,
+  ModalProps
 };
 
 
